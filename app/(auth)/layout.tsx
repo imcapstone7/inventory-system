45489ecc70d3aed3a35f75ff14aa33b0ee4c1693
Bryan Revilla Tabajonda <tabajondabryan7@gmail.com>
2024-06-01 13:40:46 +0800

    removed the useState

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,22 +1,8 @@
-"use client"
-
-import { useEffect, useState } from "react";
-
 export default function PageLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
-
-    const [isMounted, setIsMounted] = useState(false);
-
-    useEffect(() => {
-        setIsMounted(true);
-    }, []);
-
-    if (!isMounted) {
-        return null;
-    }
 
     return (
         <div className="mx-8 lg:mx-0">
